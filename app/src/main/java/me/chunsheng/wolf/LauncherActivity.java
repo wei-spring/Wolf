eb58ab7c9fd2d4f487fb106c8a9bad223b60d087
wei-spring <weichunsheng3611@gmail.com>
2016-06-05 22:51:42 +0800

    启动页面欢迎视频播放

---
+++ app/src/main/java/me/chunsheng/wolf/LauncherActivity.java
@@ -0,0 +1,105 @@
+package me.chunsheng.wolf;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LauncherActivity extends Activity {
+
+
+    public static final String VIDEO_NAME = "launcher_video.mp4";
+
+    private VideoView mVideoView;
+
+    private ViewGroup contianer;
+
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        setContentView(R.layout.activity_launcher);
+
+        initView();
+
+        File videoFile = getFileStreamPath(VIDEO_NAME);
+        if (!videoFile.exists()) {
+            videoFile = copyVideoFile();
+        }
+
+        playVideo(videoFile);
+
+    }
+
+    private void initView() {
+        contianer = (ViewGroup) findViewById(R.id.container);
+        mVideoView = (VideoView) findViewById(R.id.videoView);
+        mVideoView.setAlpha((float) 0.7);
+        appName = (TextView) findViewById(R.id.appName);
+    }
+
+
+    private void playVideo(File videoFile) {
+        mVideoView.setVideoPath(videoFile.getPath());
+        mVideoView.setLayoutParams(new RelativeLayout.LayoutParams(-1, -1));
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+                mediaPlayer.start();
+            }
+        });
+    }
+
+
+    private File copyVideoFile() {
+        File videoFile;
+        try {
+            FileOutputStream fos = openFileOutput(VIDEO_NAME, MODE_PRIVATE);
+            InputStream in = getResources()
+                    .openRawResource(R.raw.launcher_video);
+            byte[] buff = new byte[1024];
+            int len = 0;
+            while ((len = in.read(buff)) != -1) {
+                fos.write(buff, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        videoFile = getFileStreamPath(VIDEO_NAME);
+        if (!videoFile.exists())
+            throw new RuntimeException(
+                    "video file has problem, are you sure you have welcome_video.mp4 in res/raw folder?");
+        return videoFile;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mVideoView.stopPlayback();
+    }
+}
